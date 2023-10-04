329bbf16ae0f7227a298fe7038250c3a43e5614f
Rutu004 <131948536+Rutu004@users.noreply.github.com>
2023-10-04 09:52:46 +0530

    Add files via upload

---
+++ campainpage1.java
@@ -0,0 +1,30 @@
+package pom;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class campainpage1 {
+
+	@FindBy (id="Campaigns_listView_basicAction_LBL_ADD_RECORD")
+	private WebElement record;
+
+	public WebElement getRecord() {
+		return record;
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
